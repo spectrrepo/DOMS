4c37cd57cf44266eb1d147df2a7fa54ce6e10ffc
spectrrepo <georgy.voldaev@yandex.ru>
2016-12-20 13:28:54 +0300

    e

--- resources/assets/js/popup.js
+++ resources/assets/js/popup.js
@@ -1,216 +1,11 @@
 $( document ).ready(function() {
-  var queue = 1,
-      dataURL = 0,
-      csrftoken = $('meta[name=_token]').attr('content'),
-      sortSort = $('input[name=sortSort]').val(),
-      styleSort = $('input[name=styleSort]').val(),
-      roomSort = $('input[name=roomSort]').val(),
-      colorSort = $('input[name=colorSort]').val();
+  function addLabel() {
 
-  $('.close').on('click', function () {
-    $('.click-ready').removeClass('active-menu-item');
-    $('.sidebar-modal').fadeOut();
-  });
+  }
+  function deleteLabel() {
 
-  // placements sort control
-  $('#placements span.click-ready').on('click', function () {
-    if ($(this).next().is('.block-view')){
-      $(this).next().removeClass('block-view');
-      $(this).removeClass('active-menu-item');
-    }
-    else {
-      $('span.click-ready').removeClass('active-menu-item');
-      $('span.click-ready').next().removeClass('block-view');
-      $(this).next().addClass('block-view');
-      $(this).addClass('active-menu-item');
-    }
-  });
-
-
-  $('#placements .item-moodal-sidebar').on('click', function () {
-    var id = $(this).data('id');
-    var title = $(this).children('.item-modal-text').text();
-
-    if (!($('#placements .item-moodal-sidebar').is('[data-queue=3]'))) {
-        if($(this).is('[data-queue]')) {
-          $('#placements .choose-sort-item[data-list='+$(this).data('queue')+']').remove();
-          $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
-          +']').children('.choose-ico').removeClass('active-choose-ico');
-          $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
-          +']').removeAttr('data-queue');
-          $('#placements .item-moodal-sidebar[data-queue=2]').attr('data-queue', 1);
-          $('#placements .item-moodal-sidebar[data-queue=3]').attr('data-queue', 2);
-          $('#placements .choose-sort-item[data-list=2]').attr('data-list', 1);
-          $('#placements .choose-sort-item[data-list=3]').attr('data-list', 2);
-          var deleteURL = $('#placements .item-moodal-sidebar[data-queue='+
-          $(this).data('queue')+']').data('url');
-          console.log(deleteURL);
-          dataURL = dataURL.replace(deleteURL+',', '');
-          history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-          if ($('#placements .item-moodal-sidebar[data-queue]').length === 2){
-            queue = 3;
-
-          }else if ($('#placements .item-moodal-sidebar[data-queue]').length === 1) {
-            queue = 2;
-          }else if ($('#placements .item-moodal-sidebar[data-queue]').length === 0) {
-            queue = 1;
-            dataURL = 0;
-            history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-
-          }
-          $('input[name=roomSorting]').val(dataURL);
-        }else {
-          if ($('#placements .item-moodal-sidebar[data-queue]').length === 0) {
-
-            dataURL =$(this).data('url');
-            history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-
-            $(this).attr('data-queue', queue);
-            $(this).children('.choose-ico').addClass('active-choose-ico');
-
-            $('<li class="choose-sort-item" data-list="'+queue+'" '+
-            'data-url="'+$(this).data('url')+'">'+
-            '<span class="name-sort-item">'+title+'</span>' +
-            '<i class="close-sort-item">×</i>' +
-            '</li>').appendTo('#placements');
-
-            ++queue;
-            $('input[name=roomSorting]').val(dataURL);
-          }else {
-            dataURL += ','+$(this).data('url');
-            history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-
-            $(this).attr('data-queue', queue);
-            $(this).children('.choose-ico').addClass('active-choose-ico');
-
-            $('<li class="choose-sort-item" data-list="'+queue+'" '+
-            'data-url="'+$(this).data('url')+'">'+
-            '<span class="name-sort-item">'+title+'</span>' +
-            '<i class="close-sort-item">×</i>' +
-            '</li>').appendTo('#placements');
-
-            ++queue;
-            $('input[name=roomSorting]').val(dataURL);
-          }
-        }
-    } else {
-        if ($(this).is('[data-queue]')) {
-          $('#placements .choose-sort-item[data-list='+$(this).data('queue')+']').remove();
-          var deleteURL = $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
-          +']').data('url');
-          $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
-          +']').children('.choose-ico').removeClass('active-choose-ico');
-          $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
-          +']').removeAttr('data-queue');
-          $('#placements .item-moodal-sidebar[data-queue=2]').attr('data-queue', 1);
-          $('#placements .item-moodal-sidebar[data-queue=3]').attr('data-queue', 2);
-          $('#placements .choose-sort-item[data-list=2]').attr('data-list', 1);
-          $('#placements .choose-sort-item[data-list=3]').attr('data-list', 2);
-          if ($('#placements .item-moodal-sidebar[data-queue]').length === 2){
-            queue = 3;
-            dataURL = dataURL.replace(deleteURL+',', '');
-            history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-
-          }
-          if ($('#placements .item-moodal-sidebar[data-queue]').length  === 1) {
-            queue = 2;
-            dataURL = dataURL.replace(deleteURL, '');
-            history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-
-          }
-          if ($('#placements .item-moodal-sidebar[data-queue]').length  === 0) {
-            queue = 1;
-            dataURL = 0;
-            history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-
-          }
-          $('input[name=roomSorting]').val(dataURL);
-        }else {
-
-          var replaceString = $(this).data('url');
-          var replace = $('#placements .item-moodal-sidebar[data-queue=1]').data('url');
-          dataURL = dataURL.replace(replace, replaceString);
-          history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-
-          $('#placements .item-moodal-sidebar[data-queue=1]').children('.choose-ico').removeClass('active-choose-ico');
-          $('#placements .item-moodal-sidebar[data-queue=1]').removeAttr('data-queue');
-          $('#placements .item-moodal-sidebar[data-queue=2]').attr('data-queue', 1);
-          $('#placements .item-moodal-sidebar[data-queue=3]').attr('data-queue', 2);
-
-          $(this).attr('data-queue', 3);
-          $(this).children('.choose-ico').addClass('active-choose-ico');
-
-          $('#placements .choose-sort-item[data-list=1]').remove();
-          $('#placements .choose-sort-item[data-list=2]').attr('data-list', 1);
-          $('#placements .choose-sort-item[data-list=3]').attr('data-list', 2);
-          $('<li class="choose-sort-item" data-list="'+3+'" '+
-          'data-url="'+$(this).data('url')+'">'+
-          '<span class="name-sort-item">'+title+'</span>' +
-          '<i class="close-sort-item">×</i>' +
-          '</li>').appendTo('#placements');
-
-          --queue;
-          $('input[name=roomSorting]').val(dataURL);
-        }
-    }
-    $('.choose-sort-item').click(function () {
-      $('#placements .item-moodal-sidebar[data-queue='+
-      $(this).data('list') +']').children('.active-choose-ico')
-                                .removeClass('active-choose-ico');
-      $('#placements .item-moodal-sidebar[data-queue='+
-      $(this).data('list') +']').removeAttr('data-queue');
-      $(this).remove();
-
-      if ($('#placements .item-moodal-sidebar[data-queue]').length === 2){
-        queue = 3
-      }
-      if ($('#placements .item-moodal-sidebar[data-queue]').length === 1) {
-        queue = 2;
-        if ($(this).data('list') === 1){
-          alert('[eq]');
-          var deleteURL = $(this).data('url');
-          dataURL = dataURL.replace(deleteURL+',', '');
-          history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-          $('input[name=roomSorting]').val(dataURL);
-        }else {
-          var deleteURL = $(this).data('url');
-          dataURL = dataURL.replace(','+deleteURL, '');
-          history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-          $('input[name=roomSorting]').val(dataURL);
-        }
-      }
-      if ($('#placements .item-moodal-sidebar[data-queue]').length === 0) {
-        queue = 1;
-        dataURL = 0;
-        history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
-        $('input[name=roomSorting]').val(dataURL);
-      }
-      $.ajax({
-        type:'POST',
-        data: {
-                  '_token'  : csrftoken,
-                  'sortSort': $('input[name=sortSorting]').val(),
-                  'styleSort': $('input[name=styleSorting]').val(),
-                  'roomSort': $('input[name=roomSorting]').val(),
-                  'colorSort': $('input[name=colorSorting]').val()
-        },
-        url:'/load_sort_photo',
-
-        success: function (data) {
-          $('#pole').empty();
-          for(var i=0; i<data.length; i++) {
-          $( '<a href="/photo/id=['+data[i].id+'],room=[0],styles=[0],colors=[0],sort=[""]"'+
-                'class="item-gallery" data-grid-prepared="true"'+
-                'style="position:absolute;">'+
-               '<div class="uk-panel-box">'+
-                 '<img src="'+data[i].min_path+'">'+
-               '</div>'+
-             '</a>').appendTo('#pole');
-
-          }
-        }
-      });
-    });
+  }
+  function ajaxRequest() {
     $.ajax({
       type:'POST',
       data: {
@@ -236,7 +31,252 @@ $( document ).ready(function() {
         }
       }
     });
+  }
+  function addActive() {
+    if ($(this).next().is('.block-view')){
+      $(this).next().removeClass('block-view');
+      $(this).removeClass('active-menu-item');
+    }
+    else {
+      $('span.click-ready').removeClass('active-menu-item');
+      $('span.click-ready').next().removeClass('block-view');
+      $(this).next().addClass('block-view');
+      $(this).addClass('active-menu-item');
+    }
+  }
+
+  function addCheck() {
+
+  }
+  function commonLabel() {
+    $('<li class="choose-sort-item" data-list="'+queue+'" '+
+    'data-url="'+$(this).data('url')+'">'+
+    '<span class="name-sort-item">'+title+'</span>' +
+    '<i class="close-sort-item">×</i>' +
+    '</li>');
+  }
+  function ajaxCallback () {
+
+  }
+  functioi
+  //======================================================>>>>>>>>>>>>>>>>>>
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  // =====================================================>>>>>>>>>>>>>>>>>>
+
+  var queue = 1,
+      dataURL = 0,
+      csrftoken = $('meta[name=_token]').attr('content'),
+      sortSort = $('input[name=sortSorting]').val(),
+      styleSort = $('input[name=styleSorting]').val(),
+      roomSort = $('input[name=roomSorting]').val(),
+      colorSort = $('input[name=colorSorting]').val();
+
+  $('.close').on('click', function () {
+    $('.click-ready').removeClass('active-menu-item');
+    $('.sidebar-modal').fadeOut();
   });
+
+  // placements sort control
+  $('#placements span.click-ready').on('click', function () {
+
+  });
+
+
+  $('#placements .item-moodal-sidebar').on('click', function () {
+    sortSort = $('input[name=sortSorting]').val(),
+    styleSort = $('input[name=styleSorting]').val(),
+    roomSort = $('input[name=roomSorting]').val(),
+    colorSort = $('input[name=colorSorting]').val();
+    var id = $(this).data('id');
+    var title = $(this).children('.item-modal-text').text();
+
+    if (!($('#placements .item-moodal-sidebar').is('[data-queue=3]'))) {
+        if($(this).is('[data-queue]')) {
+          $('#placements .choose-sort-item[data-list='+$(this).data('queue')+']').remove();
+          $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
+          +']').children('.choose-ico').removeClass('active-choose-ico');
+          $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
+          +']').removeAttr('data-queue');
+          $('#placements .item-moodal-sidebar[data-queue=2]').attr('data-queue', 1);
+          $('#placements .item-moodal-sidebar[data-queue=3]').attr('data-queue', 2);
+          $('#placements .choose-sort-item[data-list=2]').attr('data-list', 1);
+          $('#placements .choose-sort-item[data-list=3]').attr('data-list', 2);
+          var deleteURL = $('#placements .item-moodal-sidebar[data-queue='+
+          $(this).data('queue')+']').data('url');
+          console.log(deleteURL);
+          dataURL = dataURL.replace(deleteURL+',', '');
+          history.pushState(null, null, 'room=['+dataURL+'],styles=[0],colors=[0],sort=[""]');
+          if ($('#placements .item-moodal-sidebar[data-queue]').length === 2){
+            queue = 3;
+          }else if ($('#placements .item-moodal-sidebar[data-queue]').length === 1) {
+            queue = 2;
+          }else if ($('#placements .item-moodal-sidebar[data-queue]').length === 0) {
+            queue = 1;
+            dataURL = 0;
+            history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+
+          }
+          $('input[name=roomSorting]').val(dataURL);
+        }else {
+          if ($('#placements .item-moodal-sidebar[data-queue]').length === 0) {
+
+            dataURL =$(this).data('url');
+            history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+
+            $(this).attr('data-queue', queue);
+            $(this).children('.choose-ico').addClass('active-choose-ico');
+
+            commonLabel.appendTo('#placements');
+
+            ++queue;
+            $('input[name=roomSorting]').val(dataURL);
+          }else {
+            dataURL += ','+$(this).data('url');
+            history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+
+            $(this).attr('data-queue', queue);
+            $(this).children('.choose-ico').addClass('active-choose-ico');
+
+            commonLabel.appendTo('#placements');
+
+            ++queue;
+            $('input[name=roomSorting]').val(dataURL);
+          }
+        }
+    } else {
+        if ($(this).is('[data-queue]')) {
+          $('#placements .choose-sort-item[data-list='+$(this).data('queue')+']').remove();
+          var deleteURL = $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
+          +']').data('url');
+          $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
+          +']').children('.choose-ico').removeClass('active-choose-ico');
+          $('#placements .item-moodal-sidebar[data-queue='+$(this).data('queue')
+          +']').removeAttr('data-queue');
+          $('#placements .item-moodal-sidebar[data-queue=2]').attr('data-queue', 1);
+          $('#placements .item-moodal-sidebar[data-queue=3]').attr('data-queue', 2);
+          $('#placements .choose-sort-item[data-list=2]').attr('data-list', 1);
+          $('#placements .choose-sort-item[data-list=3]').attr('data-list', 2);
+          if ($('#placements .item-moodal-sidebar[data-queue]').length === 2){
+            queue = 3;
+            dataURL = dataURL.replace(deleteURL+',', '');
+            history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+
+          }
+          if ($('#placements .item-moodal-sidebar[data-queue]').length  === 1) {
+            queue = 2;
+            dataURL = dataURL.replace(deleteURL, '');
+            history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+
+          }
+          if ($('#placements .item-moodal-sidebar[data-queue]').length  === 0) {
+            queue = 1;
+            dataURL = 0;
+            history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+
+          }
+          $('input[name=roomSorting]').val(dataURL);
+        }else {
+
+          var replaceString = $(this).data('url');
+          var replace = $('#placements .item-moodal-sidebar[data-queue=3]').data('url');
+          dataURL = dataURL.replace(replace, replaceString);
+          history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+
+          $('#placements .item-moodal-sidebar[data-queue=1]').children('.choose-ico').removeClass('active-choose-ico');
+          $('#placements .item-moodal-sidebar[data-queue=1]').removeAttr('data-queue');
+          $('#placements .item-moodal-sidebar[data-queue=2]').attr('data-queue', 1);
+          $('#placements .item-moodal-sidebar[data-queue=3]').attr('data-queue', 2);
+
+          $(this).attr('data-queue', 3);
+          $(this).children('.choose-ico').addClass('active-choose-ico');
+
+          $('#placements .choose-sort-item[data-list=1]').remove();
+          $('#placements .choose-sort-item[data-list=2]').attr('data-list', 1);
+          $('#placements .choose-sort-item[data-list=3]').attr('data-list', 2);
+          commonLabel.appendTo('#placements');
+
+          --queue;
+          $('input[name=roomSorting]').val(dataURL);
+        }
+    }
+    $('.choose-sort-item').click(function () {
+      $('#placements .item-moodal-sidebar[data-queue='+
+      $(this).data('list') +']').children('.active-choose-ico')
+                                .removeClass('active-choose-ico');
+      $('#placements .item-moodal-sidebar[data-queue='+
+      $(this).data('list') +']').removeAttr('data-queue');
+      $(this).remove();
+
+      if ($('#placements .item-moodal-sidebar[data-queue]').length === 2){
+        queue = 3
+      }
+      if ($('#placements .item-moodal-sidebar[data-queue]').length === 1) {
+        queue = 2;
+        if ($(this).data('list') === 1){
+          alert('[eq]');
+          var deleteURL = $(this).data('url');
+          dataURL = dataURL.replace(deleteURL+',', '');
+          history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+          $('input[name=roomSorting]').val(dataURL);
+        }else {
+          var deleteURL = $(this).data('url');
+          dataURL = dataURL.replace(','+deleteURL, '');
+          history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+          $('input[name=roomSorting]').val(dataURL);
+        }
+      }
+      if ($('#placements .item-moodal-sidebar[data-queue]').length === 0) {
+        queue = 1;
+        dataURL = 0;
+        history.pushState(null, null, 'room=['+dataURL+'],styles=['+styleSort+'],colors=['+colorSort+'],sort=['+sortSort+']');
+        $('input[name=roomSorting]').val(dataURL);
+      }
+      ajaxRequest;
+    });
+    ajaxRequest;
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // styles sort control
   $('#styles span.click-ready').on('click', function () {
     if ($(this).next().is('.block-view')){
