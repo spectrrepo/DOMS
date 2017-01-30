0908215c4a084a0c2f3df3fd72bc4c566bfbb57b
spectrrepo <georgy.voldaev@yandex.ru>
2017-01-30 10:01:42 +0300

    also fix absolut path

--- resources/views/helpers/sidebar_profile.blade.php
+++ resources/views/helpers/sidebar_profile.blade.php
@@ -1,10 +1,10 @@
 <!-- TODO:add link -->
 <nav class="sidebar" style="margin-top:-16px;">
   <ul>
-    <li class="menu-item-profile {{ preg_match('/profile/'.Auth::id(), URL::current()) ? 'menu-item-profile-active' : '' }}"><a href="{{ URL::to('profile/'.Auth::id()) }}">Моя страница</a></li>
-    <li class="menu-item-profile {{ preg_match('/add/photo'.Auth::id(), URL::current()) ? 'menu-item-profile-active' : '' }}"><a href="{{ URL::route('add') }}">Добавить изображение</a></li>
-    <li class="menu-item-profile {{ preg_match('/edit/user'.Auth::id(), URL::current()) ? 'menu-item-profile-active' : '' }}"><a href="{{ URL::route('edit') }}">Редактировать профиль</a></li>
-    <li class="menu-item-profile {{ preg_match('/liked/photo'.Auth::id(), URL::current()) ? 'menu-item-profile-active' : '' }}"><a href="{{ URL::route('liked') }}">Избранное</a></li>
+    <li class="menu-item-profile {{ preg_match('[/profile/'.Auth::id().']', URL::current()) ? 'menu-item-profile-active' : '' }}"><a href="{{ URL::to('profile/'.Auth::id()) }}">Моя страница</a></li>
+    <li class="menu-item-profile {{ preg_match('[/add/photo]', URL::current()) ? 'menu-item-profile-active' : '' }}"><a href="{{ URL::route('add') }}">Добавить изображение</a></li>
+    <li class="menu-item-profile {{ preg_match('[/edit/user]', URL::current()) ? 'menu-item-profile-active' : '' }}"><a href="{{ URL::route('edit') }}">Редактировать профиль</a></li>
+    <li class="menu-item-profile {{ preg_match('[/liked/photo]', URL::current()) ? 'menu-item-profile-active' : '' }}"><a href="{{ URL::route('liked') }}">Избранное</a></li>
     @if (Auth::user()->status === 'moderator')
       <li class="menu-item-profile"><a href="{{ URL::route('verified') }}">Изображения</a></li>
       <li class="menu-item-profile"><a href="{{ URL::route('rooms_edit') }}">Элементы сортировки</a></li>
