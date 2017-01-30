b00adf354ade98656bb95445b9845c196ed35617
spectrrepo <georgy.voldaev@yandex.ru>
2017-01-30 11:13:44 +0300

    fix paggination index page

--- resources/assets/js/ajax-pagination.js
+++ resources/assets/js/ajax-pagination.js
@@ -3,10 +3,11 @@ $( document ).ready(function() {
   $('.b-next-page').on('click', function() {
       var csrftoken = $('meta[name=_token]').attr('content'),
           lastIdJS = $('#pole').children('.item-gallery:last-child').index(),
-          sortSorting = $('input[name=sortSorting]').val();
-          styleSorting = $('input[name=styleSorting]').val();
-          roomSorting = $('input[name=roomSorting]').val();
-          colorSorting = $('input[name=colorSorting]').val();
+          sortSorting = $('input[name=sortSorting]').val(),
+          styleSorting = $('input[name=styleSorting]').val(),
+          roomSorting = $('input[name=roomSorting]').val(),
+          colorSorting = $('input[name=colorSorting]').val(),
+          tagSorting = $('input[name=tagSorting]').val();
 
       $.ajax({
           type:'POST',
@@ -16,23 +17,24 @@ $( document ).ready(function() {
                   'sortSorting': sortSorting,
                   'styleSorting': styleSorting,
                   'roomSorting': roomSorting,
-                  'colorSorting': colorSorting
+                  'colorSorting': colorSorting,
+                  'tagSorting': tagSorting
           },
           url:'/pagination_index',
 
           success: function (data) {
-                data.forEach( function(item, i, data) {
-                  $('<a href="/photo/id=['+item.id+
+            for(var i=0; i<data.length; i++) {
+                  $('<a href="/photo/id=['+data[i].id+
                       '],room=['+$('input[name=roomSort]').val()+
                       '],styles=['+$('input[name=styleSort]').val()+
                       '],colors=['+$('input[name=colorSort]').val()+
                       '],sort=['+$('input[name=sortSort]').val()+
                       ']" class="item-gallery" data-grid-prepared="true"style="position:absolute;">' +
                       '<div class="uk-panel-box">' +
-                        '<img src="'+item.min_path+'">'+
+                        '<img src="'+data[i].min_path+'">'+
                        '</div>' +
                      '</a>').appendTo('.uk-grid-width-small-1-2');
-                   });
+            }
           }
       });
   });
