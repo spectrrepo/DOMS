b268661b0cbcdafd42f2522a4b27513d52b8d32f
spectrrepo <georgy.voldaev@yandex.ru>
2017-01-31 14:59:49 +0300

    message

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Codesleeve\LaravelStapler\Providers\L5ServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
         SocialiteProviders\Manager\ServiceProvider::class,
         /*
          * Application Service Providers...
